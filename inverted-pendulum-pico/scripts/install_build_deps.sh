0b1fc49c0faf8ebda6c8695e1628b8fea125eac1
mc18g13 <matthewcaswell@hotmail.co.uk>
2021-02-04 22:18:48 +0000

    begin adding debug for pico

---
+++ inverted-pendulum-pico/scripts/install_build_deps.sh
@@ -0,0 +1,1 @@
+apt-get install -y git cmake gcc-arm-none-eabi gcc g++ python3
---
+++ inverted-pendulum-pico/scripts/install_dev_deps.sh
@@ -0,0 +1,3 @@
+DEPS="openocd build-essential texinfo libtool libftdi-dev libusb-1.0-0-dev"
+
+sudo apt-get install $DEPS 
