2669d7b527c8170188fbbc3f57103ba7ebeb51bd
mc18g13 <matthewcaswell@hotmail.co.uk>
2021-02-06 12:20:21 +0000

    add pico step file for designs and add app to read rotary encoder

--- rotary-inverted-pendulum-pico/main.c
+++ rotary-inverted-pendulum-pico/main.c
@@ -2,14 +2,24 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-bool reserved_addr(uint8_t addr) {
-    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
-}
+#define I2C_PORT i2c0
  
 #define LED_PIN 25
 #define SDA_PIN 4
 #define SCL_PIN 5
 #define I2C_FREQ_HZ 400 * 1000
+
+static void read_raw(int16_t* angle) {
+    const uint8_t ANGLE_ADDRESS_MSB = 0x0E;
+    const uint8_t DEVICE_ADDRESS = 0x36;
+
+    uint8_t buffer[2];
+
+    i2c_write_blocking(I2C_PORT, DEVICE_ADDRESS, &ANGLE_ADDRESS_MSB, 1, true);
+    i2c_read_blocking(I2C_PORT, DEVICE_ADDRESS, buffer, sizeof(uint16_t), false);
+
+    *angle = (buffer[0] << 8) | buffer[1];
+}
 
 int main() {
     stdio_init_all();
@@ -24,34 +34,12 @@ int main() {
     gpio_pull_up(SDA_PIN);
     gpio_pull_up(SCL_PIN);
 
+    int16_t angle;
     while (1) {
-      printf("\nI2C Bus Scan\n");
-      printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-  
-      for (int addr = 0; addr < (1 << 7); ++addr) {
-          if (addr % 16 == 0) {
-              printf("%02x ", addr);
-          }
-  
-          // Perform a 1-byte dummy read from the probe address. If a slave
-          // acknowledges this address, the function returns the number of bytes
-          // transferred. If the address byte is ignored, the function returns
-          // -1.
-  
-          // Skip over any reserved addresses.
-          int ret;
-          uint8_t rxdata;
-          if (reserved_addr(addr))
-              ret = PICO_ERROR_GENERIC;
-          else
-              ret = i2c_read_blocking(i2c0, addr, &rxdata, 1, false);
-  
-          printf(ret < 0 ? "." : "@");
-          printf(addr % 16 == 15 ? "\n" : "  ");
-      }
+      read_raw(&angle);
       gpio_put(LED_PIN, !gpio_get(LED_PIN));
-      printf("Done.\n");
-      // sleep_ms(1000);
+      printf("angle = %d\n", angle);
+      // sleep_ms(100);
     }
 
     return 0;
